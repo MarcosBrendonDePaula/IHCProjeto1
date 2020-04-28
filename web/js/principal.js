7b7fc268600d65ac6ce4cb359d4d216104e01eae
Marcos Brendon <marcos15975321@gmail.com>
2020-04-28 08:19:43 -0300

    jquery
    Adicionado

--- web/js/principal.js
+++ web/js/principal.js
@@ -4,9 +4,20 @@ const LoginDisplay = document.querySelector("#LoginDisplay")
 const BtnLogar = document.querySelector("#btnLogin")
 const BtnRegistrar = document.querySelector("#btnregistrar")
 
+function createFirstCokkie(){
+    users = document.cookie.split("")
+
+}
+window.flip = function(flip) {
+    $('#cube').removeClass(); 
+    $('#cube').addClass(flip);   
+}
 function PrepareUser(){
     if(sessionStorage.getItem("User")!=undefined){
         let btn = document.createElement('button')
+        btn.style.color="white";
+        btn.style.border="1px solid white"
+        btn.style.marginRight="2vw";
         btn.innerText ="Logado :> "+ sessionStorage.getItem("User")
         btn.classList.add('mdl-button')
         btn.classList.add('mdl-js-button')
@@ -14,9 +25,6 @@ function PrepareUser(){
         LoginInteface.appendChild(btn)
     }
 }
-
-
-
 
 BtnLogar.addEventListener("click",()=>{ 
     LoginDisplay.classList.toggle("hidden")
