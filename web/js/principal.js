ceaaa8e97ec3f215801bfafd792e86fdd160338f
Marcos Brendon <marcos15975321@gmail.com>
2020-04-29 09:57:06 -0300

    Simulação de usuario criada

--- web/js/principal.js
+++ web/js/principal.js
@@ -7,36 +7,7 @@ const BtnRegistrar = document.querySelector("#btnregistrar")
 //----Manipulando Usuarios
 const singUpButton = document.querySelector("#singUpButton")
 
-
-var Login_Usuarios=[]
 var Usuarios={}
-
-function setBasicUsers(){
-    var userlist=[]
-    userlist.push({"login":"Marcos","Senha":"azx0025"})
-    userlist.push({"login":"Henri","Senha":"henrixss"})
-    userlist.push({"login":"Eric","Senha":"PacoLoco"})
-    document.cookie=JSON.stringify({"listUsers":userlist})+";expires="+new Date(2030,5,01).toGMTString()+";"
-}
-
-function LoadCokkie(){
-    if(document.cookie.length <= 1)
-        setBasicUsers()
-    let json = JSON.parse(document.cookie)
-    for (let i of json.listUsers){
-        Login_Usuarios.push(i.login)
-        Usuarios[i.login]=i.Senha
-    }
-}
-
-function UpdateUsers(){
-    let Uservector=[]
-    for(let i of Login_Usuarios){
-        console.log(i)
-        Uservector.push({'login':i,'Senha':Usuarios[i]})
-    }
-    document.cookie=JSON.stringify({"listUsers":Uservector})+";expires="+new Date(2030,5,01).toGMTString()+";"
-}
 
 //Cadastrando Um novo Usuario
 singUpButton.addEventListener('click',()=>{
@@ -44,24 +15,22 @@ singUpButton.addEventListener('click',()=>{
     let LoginCampo = document.querySelector(".signLogin")
 
     if((passwCampos[0].value==passwCampos[1].value) & Usuarios[LoginCampo.value]==undefined){
-        Login_Usuarios.push(LoginCampo.value)
-        Usuarios[LoginCampo.value]=passwCampos[1].value
-        UpdateUsers()
+        Usuarios[LoginCampo.value]={"passw":passwCampos[1].value,"cart":[]}
+        sessionStorage.setItem("Users",JSON.stringify(Usuarios))
+        sessionStorage.setItem("User",JSON.stringify({"login":LoginCampo.value,"passw":passwCampos[1].value,"cart":[]}))
+        PrepareUser()
     }
 })
 //---------------------------------------
 
-window.flip = function(flip) {
-    $('#cube').removeClass(); 
-    $('#cube').addClass(flip);   
-}
+
 function PrepareUser(){
     if(sessionStorage.getItem("User")!=undefined){
         let btn = document.createElement('button')
         btn.style.color="white";
         btn.style.border="1px solid white"
         btn.style.marginRight="2vw";
-        btn.innerText ="Logado :> "+ sessionStorage.getItem("User")
+        btn.innerText ="Logado :> "+ JSON.parse(sessionStorage.getItem("User")).login
         btn.classList.add('mdl-button')
         btn.classList.add('mdl-js-button')
         LoginInteface.innerHTML=""
@@ -83,7 +52,10 @@ BtnLogar.addEventListener("click",()=>{
     LoginDisplay.addEventListener("click",ocultar)
 })
 
-
-
+window.flip = function(flip) {
+    $('#cube').removeClass(); 
+    $('#cube').addClass(flip);   
+}
+if(sessionStorage.getItem("Users")!=undefined)
+    Usuarios = JSON.parse(sessionStorage.getItem("Users"))
 PrepareUser()
-LoadCokkie()
